4c4c17f98c680318c933762d957bd2823ffc2740
me-cinderella <mariana.encalada.ap@gmail.com>
2021-03-02 15:32:39 -0300

    feat: add footer

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-    <Header />
-    <div className="App">
-      <h1>REACT APP</h1>
-    </div>
+      <Header />
+      <div className="App">
+        <h1>REACT APP</h1>
+      </div>
+      <Footer />
     </>
   );
 }
---
+++ app/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function CustomFooter() {
+    return (
+        <footer className="fixed-bottom bg-dark text-white">
+          <div className="text-left p-3">
+            <span>Copyright © 2021.</span>
+          </div>
+        </footer>
+    )
+}
+
+export default CustomFooter;
