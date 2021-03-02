42910db79931a0d0241cbd32e5c50c04c9500908
me-cinderella <mariana.encalada.ap@gmail.com>
2021-03-02 18:01:56 -0300

    style: add body background image

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <>
       <Header />
-      <div className="App">
-        <h1>REACT APP</h1>
-      </div>
       <Footer />
     </>
   );
